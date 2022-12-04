e24ba77a9121d279adb9856fbb12d1ca21bc9ad2
monjenahuel <nahuel.monje@estudiantes.unahur.edu.ar>
2022-12-04 02:00:02 -0300

    inicializacion del repositorio

---
+++ app.js
@@ -0,0 +1,81 @@
+const express = require('express')
+
+const app = express()
+
+const {mesas} = require('./mesas/mesas.js')
+
+
+let cors = require("cors"); //Importar cors para consumir mi api
+app.use(cors()); //Usar cors
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())  
+
+/////////////////Mesas
+
+app.get('/mesas',(req,res) =>{
+    respuesta = mesas
+    if (req.query.pendiente == "true"){  
+        respuesta = mesas.filter(m => m.pedidoPendiente == true)
+    }
+    res.send(JSON.stringify(respuesta))
+})
+
+app.get('/mesas/:id',(req,res) =>{
+    const id = req.params.id
+    res.send(JSON.stringify(mesas[id-1]))
+})
+
+// app.delete('/mesas/:id',(req,res) =>{
+//     const id = req.params.id
+//     let mesaAVaciar = mesas[id-1]
+//     mesaAVaciar.productos = []
+//     mesaAVaciar.pedidoPendiente = false
+//     res.send(JSON.stringify(mesas[id-1]))
+// })
+
+////////////////////Pedidos////////////////////
+app.get('/mesas/:id/pedido',(req,res) =>{
+    const id = req.params.id
+    res.send(JSON.stringify(mesas[id-1].productos))
+})
+
+app.post('/mesas/:id/pedido',(req,res) =>{
+    try {
+        const id = req.params.id
+        let pedidoCompleto = req.body
+        let mesaPedido = mesas[id-1]
+        if(typeof(pedidoCompleto[0].precio) === typeof(27)){//typeof(pedidoCompleto[0].precio) == number
+            mesaPedido.productos = pedidoCompleto
+            mesaPedido.pedidoPendiente = true
+            res.send(JSON.stringify(mesas[id-1]))
+        }   
+    }catch(error) {
+        res.status(400)
+        console.log("El formato del pedido es incorrecto")
+        res.send('[{"nombre": "STRING","cantidad": INTEGER,"precio": INTEGER}]')
+    }
+    
+})
+
+app.delete('/mesas/:id/pedido',(req,res) =>{
+    const id = req.params.id
+    let mesaAVaciar = mesas[id-1]
+    mesaAVaciar.productos = []
+    mesaAVaciar.pedidoPendiente = false
+    res.send(JSON.stringify(mesas[id-1]))
+})
+
+// app.patch('/mesas/:id/pedido/pendiente',(req,res) =>{
+    
+// })
+
+
+
+//////////////Deploy
+
+const puerto = process.env.PORT || 3000; //Usar puerto de entorno si hay, o 3000
+
+app.listen(puerto, () => {
+    console.log('Servidor JBNM escuchando en el puerto ' + puerto)
+})
---
+++ mesas/mesas.js
@@ -0,0 +1,87 @@
+let mesas = [
+        {
+        idMesa : 1,
+        productos:[
+        {
+            nombre: "Ensalada rusa",
+            cantidad: 1,
+            precio: 450
+        },
+        {
+            nombre: "Tomates cherry griegos",
+            cantidad: 1,
+            precio: 1200
+        },
+        {
+            nombre: "Agua mineral",
+            cantidad: 2,
+            precio: 300
+        }
+        ],
+        pedidoPendiente: true
+    },
+{
+    idMesa : 2,    
+    productos:[
+        {
+        nombre: "Waffles con helado",
+        cantidad: 2,
+        precio: 1100
+        },
+    {
+        nombre: "Jugo de Naranja",
+        cantidad: 2,
+        precio: 650
+    }
+],
+    pedidoPendiente: true
+},
+{
+    idMesa : 3,    
+    productos:[],
+    pedidoPendiente: false
+},
+{
+    idMesa : 4,    
+    productos:[],
+    pedidoPendiente: false
+},
+{
+    idMesa : 5,    
+    productos:[],
+    pedidoPendiente: false
+},
+{
+    idMesa : 6,    
+    productos:[],
+    pedidoPendiente: false
+},
+{
+    idMesa : 7,    
+    productos:[],
+    pedidoPendiente: false
+},
+{
+    idMesa : 8,    
+    productos:[],
+    pedidoPendiente: false
+},
+{
+    idMesa : 9,    
+    productos:[],
+    pedidoPendiente: false
+},
+{
+    idMesa : 10,    
+    productos:[
+        {
+            nombre: "Bistec de caballo",
+            cantidad: 3,
+            precio: 2700
+        }
+    ],
+    pedidoPendiente: true
+}
+]
+
+module.exports.mesas = mesas
